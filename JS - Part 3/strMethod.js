19a0ebc0bb0d977630cbc6fc743d0bf6142f0ae1
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-26 19:12:26 -0400

    added trim and upper lower string functions

---
+++ JS - Part 3/strMethod.js
@@ -0,0 +1,23 @@
+// String Methods
+
+// --------------------------------------------------------------------------------------------
+
+// trim()
+
+let msg = "  Hello  ";
+
+// msg.trim() - Trims whitespaces from both ends of the string and returns a new one.
+console.log(msg.trim());
+
+// --------------------------------------------------------------------------------------------
+
+// toUpperCase(), toLowerCase()
+
+let str = "Random Text";
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+// --------------------------------------------------------------------------------------------
+
+
