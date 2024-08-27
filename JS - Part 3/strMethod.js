ea680f197ddbf4a4f2695dfd9f9aa0a8520c1b53
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-27 09:06:32 -0400

    added indexOf function

--- JS - Part 3/strMethod.js
+++ JS - Part 3/strMethod.js
@@ -2,22 +2,30 @@
 
 // --------------------------------------------------------------------------------------------
 
-// trim()
+// trim() - Trims whitespaces from both ends of the string and returns a new one.
 
 let msg = "  Hello  ";
 
-// msg.trim() - Trims whitespaces from both ends of the string and returns a new one.
 console.log(msg.trim());
 
 // --------------------------------------------------------------------------------------------
 
 // toUpperCase(), toLowerCase()
 
-let str = "Random Text";
+let str = "Random String";
 
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
 
 // --------------------------------------------------------------------------------------------
 
+// indexOf("subString") - Returns the "first index of occurrence" of some value in the string. Or gives -1 if not found
+
+str = "ILoveCoding";
+console.log(str.indexOf("Coding"));
+console.log(str.indexOf("z"));
+
+// --------------------------------------------------------------------------------------------
+
+//
 
