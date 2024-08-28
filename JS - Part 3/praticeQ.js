3b8e067858aedecff783cbbf7f91484f464d4b32
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-28 16:36:23 -0400

    added practice questions for string methods

---
+++ JS - Part 3/praticeQ.js
@@ -0,0 +1,12 @@
+let msg = "help!";
+
+// Trim and update it to upper case
+console.log(msg.trim().toUpperCase());
+
+let name = "ApnaCollege"
+
+console.log(name.slice(4,9)); // Colle
+console.log(name.indexOf("na")); // 2
+console.log(name.replace("Apna","Our")); // OurCollege
+
+console.log(name.slice(4).replace("l","t")); // Cotlege
