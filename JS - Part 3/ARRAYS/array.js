0bcfeac59db6c0bca9066908ae56ae2e28d87707
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-29 19:37:44 -0400

    added length and typeof

--- JS - Part 3/ARRAYS/array.js
+++ JS - Part 3/ARRAYS/array.js
@@ -1,3 +1,11 @@
 let students = ["Aman", "Shradha", "Tripti", "Divyanshu"];
 
-console.log(students);
+console.log(students); // Displaying the array students
+console.log(students[0]); // Accessing an element in the array
+console.log(students.length); // Length of the array (length method)
+console.log(typeof students); // Displaying the typeof the array - object
+
+// Array with different types
+let arr = [1, 2, "Hello", "Mello"]; // Mixed Array
+
+console.log(arr);
