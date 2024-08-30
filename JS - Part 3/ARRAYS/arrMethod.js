1ebe01e0cab981ac77ad709800e814bbf588a304
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-29 21:22:52 -0400

    added push pop shift unshift functions

---
+++ JS - Part 3/ARRAYS/arrMethod.js
@@ -0,0 +1,19 @@
+// Array Methods
+
+let car = ["Toyota", "BMW", "MARUTI", "XUV"];
+
+// Push - Add to the end
+car.push("Hundai");
+console.log(car);
+
+// Pop - Deletes form the end and returns it - this can help storing the deleted element (useful to view blocked followers on instagram)
+console.log(car.pop());
+console.log(car);
+
+// Unshift - Add to the start
+car.unshift("Hundai");
+console.log(car);
+
+// Shift - Deletes from the start and returns it - this can help storing the deleted element (useful to view blocked followers on instagram)
+console.log(car.shift());
+console.log(car);
