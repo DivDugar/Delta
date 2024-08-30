71fdc1153060cd62edd270e9a2e0fc6e59b9802c
Divyanshu Dugar <divyu2004@gmail.com>
2024-08-29 21:22:18 -0400

    added basic array operations

--- JS - Part 3/ARRAYS/array.js
+++ JS - Part 3/ARRAYS/array.js
@@ -9,3 +9,14 @@ console.log(typeof students); // Displaying the typeof the array - object
 let arr = [1, 2, "Hello", "Mello"]; // Mixed Array
 
 console.log(arr);
+console.log(arr[2][0]) // **** Accessing the character at the 0th index of the string present in the 2nd index of the array - CHAINING [][]
+console.log(arr[2].length); // *** Displaying length of the string at index 2
+
+// ARRAYS ARE MUTABLE
+let fruits = ["mango", "litchi", "apple"];
+fruits[2] = "banana"; // Updating the element at the 2nd index of the array
+
+console.log(fruits);
+
+fruits[7] = "kiwi"; // ** Adding a new element at the 7th index of the array 
+console.log(fruits); // ** Index 3-6 will be empty - [ 'mango', 'litchi', 'banana', <4 empty items>, 'kiwi' ]
