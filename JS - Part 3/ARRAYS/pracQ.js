0b7651defcb8c85717bd8fd5e282f3cf0115cd37
Divyanshu Dugar <divyu2004@gmail.com>
2024-09-18 15:52:18 -0400

    added 2d array practice problem and solution

--- JS - Part 3/ARRAYS/pracQ.js
+++ JS - Part 3/ARRAYS/pracQ.js
@@ -7,3 +7,28 @@ console.log(months.unshift('june'));
 console.log(months.unshift('july'));
 
 console.log(months);
+
+// ---------------------------------------------------
+
+let months2 = ['january', 'july', 'march', 'august']; // Make this -> [ 'july', 'june', 'march', 'august' ]
+
+console.log(months2.splice(0, 2, "july", "june")); 
+console.log(months2);
+
+// ---------------------------------------------------
+
+let languages = ['c', 'C++', 'HTML', 'javascript', 'python', 'java', 'c#', 'sql'];
+
+console.log(languages.reverse().indexOf('javascript'));
+
+// ---------------------------------------------------
+
+// Nested Arrays
+
+let ticTacToe = [['X', null, 'O'], [null, 'X', null], ['O',null, 'X']];
+
+console.log(ticTacToe);
+
+ticTacToe[0][1] = 'O';
+
+console.log(ticTacToe);
