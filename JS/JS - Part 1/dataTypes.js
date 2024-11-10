ccaa667f922088d0281f4fd2d8c2de427cf3cc49
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-10 11:47:35 -0500

    added datatypes.js file

---
+++ JS/JS - Part 1/dataTypes.js
@@ -0,0 +1,15 @@
+let intNum = 1;
+let floatNum = 1.1;
+let negNum = -1;
+
+console.log(typeof(intNum)); // number
+
+let stringVal = "Let's Check it's type";
+
+console.log(typeof(stringVal)); // string
+
+// let, var and const
+const year = 2025;
+year = 2024; // error: const variables cannot be re-assigned
+
+var a = 5; // old version of declaring variables.
